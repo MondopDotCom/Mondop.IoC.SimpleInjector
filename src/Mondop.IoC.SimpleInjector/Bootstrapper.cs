eb3cccac63d50e8da262a6df7f3f9aadba0673fb
Marcel Vroone <mvroone@it-actief.nl>
2018-01-28 21:07:36 +0100

    Implemented register collection

---
+++ src/Mondop.IoC.SimpleInjector/Bootstrapper.cs
@@ -0,0 +1,72 @@
+﻿using Mondop.Abstractions.IoC;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+
+namespace Mondop.IoC.SimpleInjector
+{
+    public class Bootstrapper
+    {
+        private readonly Dictionary<Type, IIoCModule> _modules = new Dictionary<Type, IIoCModule>();
+        private readonly List<Type> _moduleTypes = new List<Type>();
+        public readonly Container _container;
+        private readonly SimpleInjectionIoCContainer _iocContainer;
+
+        public Bootstrapper()
+        {
+            _container = new Container();
+            _iocContainer = new SimpleInjectionIoCContainer(_container);
+        }
+
+        private void RegisterDependencies(IIoCModule module)
+        {
+            foreach (var dependency in module.DependsOn)
+                RegisterModule(dependency);
+        }
+
+        private void RegisterModule(Type moduleType)
+        {
+            if(!_modules.ContainsKey(moduleType))
+            {
+                var instance = Activator.CreateInstance(moduleType) as IIoCModule;
+                instance.Register(_iocContainer);
+                _modules.Add(moduleType, instance);
+
+                RegisterDependencies(instance);
+            }
+        }
+
+        private void RegisterModules()
+        {
+            foreach (var moduleType in _moduleTypes)
+                RegisterModule(moduleType);
+        }
+
+        public void AddModule(Type moduleType)
+        {
+            _moduleTypes.Add(moduleType);
+        }
+
+        protected virtual void BeforeBootup()
+        {
+
+        }
+
+        protected virtual void AfterBootup()
+        {
+
+        }
+
+        public void Bootup()
+        {
+            BeforeBootup();
+            RegisterModules();
+            AfterBootup();
+        }
+
+        public TService GetInstance<TService>() where TService: class
+        {
+            return (TService)_container.GetInstance<TService>();
+        }
+    }
+}
---
+++ src/Mondop.IoC.SimpleInjector/SimpleInjectionIoCContainer.cs
@@ -0,0 +1,42 @@
+﻿using Mondop.Abstractions.IoC;
+using Mondop.Core;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+
+namespace Mondop.IoC.SimpleInjector
+{
+    public class SimpleInjectionIoCContainer : IIoCContainer
+    {
+        private readonly Container _container;
+
+        public SimpleInjectionIoCContainer(Container container)
+        {
+            _container = Ensure.IsNotNull(container,nameof(container));
+        }
+
+        public void Register<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            _container.Register<TService, TImplementation>();
+        }
+
+        public void Register<TService>() where TService : class
+        {
+            _container.Register<TService>();
+        }
+
+        public void RegisterSingleton<TService, TImplementation>()
+            where TService : class
+            where TImplementation : class, TService
+        {
+            _container.RegisterSingleton<TService, TImplementation>();
+        }
+
+        public void RegisterCollection<TService>(IEnumerable<Type> implementations) where TService : class
+        {
+            _container.RegisterCollection<TService>(implementations);
+        }
+    }
+}
